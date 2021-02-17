eecb58047f661677503a5250553f9021372961b8
RipSNake <seba.guzman13@gmail.com>
2021-02-17 19:45:36 -0300

    create fn to show/hide buttons and apply effects to the selected img

--- js/javascript.js
+++ js/javascript.js
@@ -24,12 +24,31 @@
 		{filter: 'saturate', value: 100},
 		{filter: 'sepia', value: 0}
 	];
-
-	
-
 */
 
 window.onload = function() { console.log('window loaded')} ;
+
+var shownImg = 'img-1'; // to save the id of the img in display. Default value 'img-1'
+
+// addEventListener to images button to show / hide un selected img
+document.getElementById('img-list').addEventListener('click',showImg);
+
+function showImg(eventSrc) {
+	let imgId = eventSrc.srcElement.attributes.href.value.slice(1);
+	let imgs = document.getElementById('img-display-layer').getElementsByTagName('img');
+	
+	for(let i = 0; i < imgs.length; i++) {
+		if(imgs[i].id === imgId) {
+			imgs[i].classList.add('show');
+			imgs[i].classList.remove('hide');
+		} else {
+			imgs[i].classList.remove('show');
+			imgs[i].classList.add('hide');
+		}
+	}
+
+	shownImg = imgId; // save the id to the shown var
+};
 
 // fetch filter list items
 var filter = document.getElementById('filter-selector').getElementsByClassName('dropdown-item');
@@ -38,7 +57,7 @@ var filterValues = [
 	{filter: 'blur', value: 0},
 	{filter: 'brightness', value: 100},
 	{filter: 'contrast', value: 100},
-	{filter: 'drop-shadow', h-shadow-value: 0, v-shadow-value: 0, spread: 0},
+	{filter: 'drop-shadow', 'h-shadow-value': 0, 'v-shadow-value': 0, spread: 0},
 	{filter: 'grayscale', value: 0},
 	{filter: 'hue-rotate', value: 0},
 	{filter: 'invert', value: 0},
@@ -47,15 +66,68 @@ var filterValues = [
 	{filter: 'sepia', value: 0}
 ];
 
-// addEventListener('click') to the filter list buttons and push them in the filterValue[] array
+// addEventListener('click') to the filter list buttons
 for(let i = 0; i < filter.length; i++) {
 	filter[i].addEventListener('click', filterShow);
 };
 
 // addEventListener to the input(range) element
-let mainInput = document.getElementsByTagName('input');
-mainInput[0].addEventListener('click', saveActualValues);
+let mainInput = document.getElementById('filterShow').getElementsByTagName('input');
+for(let i = 0; i < mainInput.length; i++) {
+	mainInput[i].addEventListener('click', saveActualValues);	
+};
 
+document.getElementById('filter-view').addEventListener('click', toggleFiltersView);
+// toggle view from one to all filters
+let showned = true;
+hideFiltersList();
+
+function toggleFiltersView() {
+	if(showned) {
+		hideFiltersList();
+	} else {
+		showFiltersList();
+	}
+};
+
+function showFiltersList() {
+	document.getElementById('filter-full-list').classList.remove('hide');
+
+	let filterInputs = document.getElementById('filterShow').children;
+	
+	let range = document.getElementById('filterShow').getElementsByTagName('input');
+
+	for(let r of range) {
+		r.disabled = true;
+	}
+	
+
+
+	for(let filter of filterInputs) {
+		filter.classList.remove('hide');
+	}
+	// Disable filters button
+	document.getElementById('filtersDropdown').classList.add('unclickable');
+	showned = true; 
+};
+
+function hideFiltersList() {
+	document.getElementById('filter-full-list').classList.add('hide');
+	let filterInputs = document.getElementById('filterShow').children;
+	
+	let range = document.getElementById('filterShow').getElementsByTagName('input');
+	for(let r of range) {
+		r.disabled = false;
+	}
+
+	for(let filter of filterInputs) {
+		if(!filter.classList.contains('selected')){
+			filter.classList.add('hide');
+		}
+	}
+	document.getElementById('filtersDropdown').classList.remove('unclickable');
+	showned = false;
+};
 
 
 // addEventListener passes as first argument the element which trigered the event
@@ -71,6 +143,7 @@ function filterShow(event) {
 	// fetch input and show name of selected filter
 	let value = document.getElementsByTagName('input');
 	value[0].name = filter.id;
+	
 
 	for(let i = 0; i < filterValues.length; i++){
 		if(value[0].name === filterValues[i].filter) {
@@ -78,33 +151,42 @@ function filterShow(event) {
 		}
 	}
 };
-
-function saveActualValues(event) {
-	let el = event.srcElement;
+// save the values in input(range) to the filterValues array
+function saveActualValues(inputValue) {
+	let el = inputValue.srcElement;
 	let saved = false;
 	for(let i = 0; i < filterValues.length && saved === false; i++) {
 		if(el.name === filterValues[i].filter) {
 			filterValues[i].value = el.value;
-			console.log('value saved');
 			saved = true;
 		};
 	};
-	applyFilters('imgElement',el.name,el.value);
-	console.log('value assigned');
+	applyFilters(shownImg,el.name,el.value);
 };
-
-function applyFilters(imgElement, filter, value) {
+// apply filter to the shown image
+function applyFilters(imgElementId, filter, value) {
+	let unit = '%';
 	if(filter === 'blur') {
-		value = value + 'px';
+		unit = 'px';
 	} else if(filter === 'hue-rotate') {
-		value = value + 'deg';
+		unit = 'deg';
 	}
 	// access the element's filter property (inside style)
-	let i = document.getElementById('img-1');
+	let i = document.getElementById(imgElementId);
 	// parse the filter string
-	let f = filter+'('+value+')';
+	let f = filter+'('+value+unit+')';
+
+	if(filter === 'drop-shadow'){
+		f = filter+'('+value+'px '+value+'px)';
+	}
 	// apply the filter
 	i.style.filter = f;
 
 	console.log('Filter '+f+' applied');
+};
+
+function populateFilterList() {
+	for(filter of filterValues) {
+
+	}
 };
